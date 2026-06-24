b485a2ba23b27b15d55d103698b3797139eaeee3
Vaibhav <Vaibhav6234>
2026-06-24 23:00:04 +0530

    Attendance page UI done

--- frontend/src/pages/Attandence.jsx
+++ frontend/src/pages/Attandence.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+import {dummyAttendanceData} from "../assets/assets"
+import Loading from "../components/Loading"
+import CheckInButton from '../components/Attendance/CheckInButton'
+import AttendanceStats from '../components/Attendance/AttendanceStats'
+import AttendanveHistory from '../components/Attendance/AttendanveHistory'
 
 const Attandence = () => {
+  const[history, setHistory] = useState([])
+  const[loading, setLoading] = useState(true)
+  const[isDeleted, setIsDeleted] = useState(false)
+
+  const fetchData = useCallback (async () =>{
+    setHistory(dummyAttendanceData)
+    setTimeout(()=>{
+      setLoading(false)
+    },1000)
+  },[])
+
+  useEffect(()=>{
+    fetchData()
+  },[fetchData]);
+
+  if(loading) return <Loading/>
+
+  const today = new Date()
+  today.setHours(0,0,0,0)
+
+  const todayRecord = history.find((r)=> new Date(r.date).toDateString() === today.toDateString())
+
   return (
-    <div>Attandence</div>
+    <div className='animate-fade-in'>
+      <div className='page-heade'>
+        <h1 className='page-title'>Attendance</h1>
+        <p className='page-subtitle'>Track your work hours and daily check-ins </p>
+      </div>
+
+      {isDeleted ? (
+        <div className='mb-8 p-6 bg-rose-50 border border-s-rose-200 rounded-2xl text-center'>
+          <p className='text-rose-600'>You can no longer clock in or orut because your employee records have been marked as deleted.</p>
+        </div>
+      ): (
+        <div className='mb-8'>
+          <CheckInButton todayRecord={todayRecord} onAction={fetchData}/>
+        </div>
+      )}
+
+      <AttendanceStats history={history}/>
+      <AttendanveHistory history={history}/>
+
+    </div>
   )
 }
 
